3db0ec4031f0774baa9e98b217089bccaed2eadc
rfnasution01 <rivaifnasution@gmail.com>
2024-04-18 16:15:23 +0700

    feat: slicing content header

--- src/features/aside-app/aside-progress.tsx
+++ src/features/aside-app/aside-progress.tsx
@@ -5,7 +5,7 @@ import { MappingTask } from './mapping-task'
 
 export function AsideProgressApp() {
   return (
-    <div className="scrollbar flex h-full flex-col gap-y-32 overflow-y-scroll bg-white p-48">
+    <div className="scrollbar flex h-full flex-col gap-y-32 overflow-y-scroll bg-white px-48 py-32">
       <HeaderWithLogo
         title="John Doe"
         logo="/img/logo.png"
