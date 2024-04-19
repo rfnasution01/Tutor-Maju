5fe65567382175c7ec645f8aad6dce03761086b2
rfnasution01 <rivaifnasution@gmail.com>
2024-04-19 20:26:10 +0700

    feat: add berita page

--- src/features/berita/mapping-berita.tsx
+++ src/features/berita/mapping-berita.tsx
@@ -1,10 +1,38 @@
 import { Button } from '@/components/Button'
+import { Pagination } from '@/components/Pagination'
+import TimeSinceUploaded from '@/libs/helpers/timeUploaded'
+import { useSearch } from '@/libs/hooks/useSearch'
+import { BeritaType } from '@/libs/interface'
+import { getSearchSlice } from '@/store/reducer/stateSearch'
+import { useGetBeritaQuery } from '@/store/slices/beritaAPI'
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 
 export function MappingBerita() {
+  const { currentPage } = useSearch()
+  const { find } = useSelector(getSearchSlice)
+  const pageSize = 3
+  const pageNumber = currentPage ?? 1
+  const [berita, setBerita] = useState<BeritaType[]>()
+
+  const { data } = useGetBeritaQuery({
+    page_size: pageSize,
+    page_number: pageNumber,
+    search: find,
+  })
+
+  useEffect(() => {
+    if (data?.data) {
+      setBerita(data?.data)
+    }
+  }, [data?.data])
+
+  const totalPage = Math.ceil(berita?.length ?? 0 / pageSize)
+
   return (
     <div className="grid h-full grid-rows-12 gap-y-32 ">
       {/* Main News */}
-      <div className="row-span-8 flex">
+      <div className="row-span-7 flex">
         <div className="flex flex-1 flex-col gap-y-24">
           {/* --- Best of --- */}
           <div className="flex">
@@ -27,6 +55,7 @@ export function MappingBerita() {
             <p className="text-slate-500">#Ethereum</p>
             <p className="text-slate-500">#Analytics</p>
           </div>
+          {/* --- Read Article --- */}
           <div className="flex">
             <Button
               rounded="rounded-xl"
@@ -40,7 +69,55 @@ export function MappingBerita() {
           <img src="/img/cosmic.png" alt="berita" className="w-[40rem]" />
         </div>
       </div>
-      <div className="row-span-4 bg-blue-200">Test</div>
+      <div className="row-span-5 flex flex-col gap-y-32">
+        <div className="grid grid-cols-12 gap-x-32">
+          <div className="col-span-4 flex flex-col gap-y-24 hover:cursor-pointer">
+            {/* --- Tag --- */}
+            <div className="flex items-center gap-x-24">
+              <p>Blockchain News</p>
+              <p className="italic text-slate-500">4 jam yang lalu</p>
+            </div>
+            {/* --- Title --- */}
+            <p className="font-roboto text-[3rem]">
+              Top Analyst Unveils Catalyst That Could Trigger Nearly 50% Surge
+              for ETH - Here's His Outlook
+            </p>
+          </div>
+          <div className="col-span-4 flex flex-col gap-y-24 hover:cursor-pointer">
+            {/* --- Tag --- */}
+            <div className="flex items-center gap-x-24">
+              <p>Blockchain News</p>
+              <p className="italic text-slate-500">12 jam yang lalu</p>
+            </div>
+            {/* --- Title --- */}
+            <p className="font-roboto text-[3rem]">
+              Over 65% of Crypto-Related Tweets and 85% of Conversations on Red
+              Markets
+            </p>
+          </div>
+          {berita?.slice(0, 1).map((item, idx) => (
+            <div
+              className="col-span-4 flex flex-col gap-y-24 hover:cursor-pointer"
+              key={idx}
+            >
+              {/* --- Tag --- */}
+              <div className="flex items-center gap-x-24">
+                <p>{item?.kategori}</p>
+                <p className="italic text-slate-500">
+                  <TimeSinceUploaded uploadTime={item?.tanggal} />
+                </p>
+              </div>
+              {/* --- Title --- */}
+              <p className="font-roboto text-[3rem]">{item?.judul}</p>
+            </div>
+          ))}
+        </div>
+
+        <Pagination
+          totalPage={totalPage === 0 ? 1 : totalPage}
+          classes="flex justify-end px-32 pb-32"
+        />
+      </div>
     </div>
   )
 }
--- src/features/pengumuman/header-pengumuman.tsx
+++ src/features/pengumuman/header-pengumuman.tsx
@@ -1,10 +1,26 @@
+import { setStateSearch } from '@/store/reducer/stateSearch'
+import { debounce } from 'lodash'
+import { useDispatch } from 'react-redux'
+
 export function HeaderPengumuman() {
+  const dispatch = useDispatch()
+
+  const handleSearch = debounce((searchValue: string) => {
+    dispatch(setStateSearch({ find: searchValue }))
+  }, 300)
+
+  const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    handleSearch(value)
+  }
+
   return (
     <div className="flex w-full items-center gap-x-32 ">
       <input
         type="text"
         className="w-full rounded-2xl border border-gray-300 p-16 text-[2rem] focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
         placeholder="Search"
+        onChange={(e) => onSearch(e)}
       />
     </div>
   )
